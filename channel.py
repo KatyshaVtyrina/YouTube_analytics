72dfd0179d3b5a69780a1a72eeb453b9b187ec17
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-02-20 22:46:33 +0300

    correction __init__, print_info add channel_id(), get_service(), to_json()

--- channel.py
+++ channel.py
@@ -8,14 +8,50 @@ class Channel:
     api_key: str = os.getenv('SKYPROAPIKEY')
 
     def __init__(self, channel_id):
-        self.channel_id = channel_id
+        self.id = channel_id
+        self.channel = self.get_service().channels().list(id=self.id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = self.channel['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.views_count = self.channel['items'][0]['statistics']['viewCount']
+
+    @property
+    def channel_id(self):
+        return self.id
+
+    @classmethod
+    # создать специальный объект для работы с API
+    def get_service(cls):
+        youtube = build('youtube', 'v3', developerKey=cls.api_key)
+        return youtube
 
     def print_info(self):
-        # создать специальный объект для работы с API
-        youtube = build('youtube', 'v3', developerKey=Channel.api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        # информация о канале
+        channel = Channel.get_service().channels().list(id=self.id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    def to_json(self, name):
+        with open(name, 'w', encoding='utf-8') as file:
+            data = {'id': self.id, 'title': self.title, 'description': self.description,
+                    'url': self.url, 'subscriber_count': self.subscriber_count, 'video_count': self.video_count,
+                    'views_count': self.views_count}
+            json.dump(data, file, indent=2, ensure_ascii=False)
 
 
 vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-vdud.print_info()
+
+# получаем значения атрибутов
+print(vdud.title)
+print(vdud.video_count)
+print(vdud.url)
+
+# менять не можем
+vdud.channel_id = 'Новое название'
+
+# можем получить объект для работы с API вне класса
+print(Channel.get_service())
+
+# создать файл 'vdud.json' в данными по каналу
+vdud.to_json('vdud.json')

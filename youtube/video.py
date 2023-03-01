f46928e4968a49570bb65b7dd359e3296f19ee64
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-03-01 22:09:40 +0300

    add class PLVideo

--- youtube/video.py
+++ youtube/video.py
@@ -34,7 +34,7 @@ class Video:
         return self.__title
 
     @property
-    def view_count(self):
+    def view_count(self) -> str:
         """Возвращает количество просмотров"""
         return self.__view_count
 
@@ -49,10 +49,49 @@ class Video:
         video = youtube.videos().list(id=video_id, part='snippet,statistics').execute()
         return video
 
-    def print_info(self):
+    def print_info_video(self) -> json:
         """Вывод информации на экран"""
         print(json.dumps(self.info_video, indent=2, ensure_ascii=False))
 
 
+class PLVideo(Video):
+
+    def __init__(self, video_id, playlist_id):
+        """Дополнительно инициализируется название плейлиста"""
+        super().__init__(video_id)
+        self.__playlist_id = playlist_id
+        self.__playlist_name = self.info_playlist['items'][0]['snippet']['title']
+
+    def __str__(self):
+        return f"{super().__str__()} ({self.__playlist_name})"
+
+    @property
+    def playlist_id(self) -> str:
+        """Возвращает id плейлиста"""
+        return self.__playlist_id
+
+    @property
+    def playlist_name(self) -> str:
+        """Возвращает имя плейлиста"""
+        return self.__playlist_name
+
+    @property
+    def info_playlist(self) -> dict:
+        """Возвращает словарь с данными по плейлисту"""
+        return self.get_playlist(self.__playlist_id)
+
+    @classmethod
+    def get_playlist(cls, playlist_id) -> dict:
+        """Получает данные о плейлисте"""
+        playlist = youtube.playlists().list(id=playlist_id, part='snippet').execute()
+        return playlist
+
+    def print_info_playlist(self) -> json:
+        """Вывод информации на экран"""
+        print(json.dumps(self.info_playlist, indent=2, ensure_ascii=False))
+
+
 video1 = Video('9lO06Zxhu88')
+video2 = PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
 print(video1)
+print(video2)

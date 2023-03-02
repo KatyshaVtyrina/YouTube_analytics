99876c7e067b8f9d96a25537c7523648467c9adf
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-03-02 18:49:05 +0300

    correction test_lt() add test_gt(), test_gt_value_error()

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -14,7 +14,12 @@ def test_add_value_error(channel_1):
         assert channel_1 + 1
 
 
-def test_lt_(channel_1, channel_2):
+def test_lt(channel_1, channel_2):
+    """Ожидается bool после сравнения экземпляров Channel"""
+    assert type(channel_1 < channel_2) is bool
+
+
+def test_gt(channel_1, channel_2):
     """Ожидается bool после сравнения экземпляров Channel"""
     assert type(channel_1 > channel_2) is bool
 
@@ -23,7 +28,16 @@ def test_lt_value_error(channel_1):
     """Ожидается исключение, если сравнивать не с Channel"""
     with pytest.raises(ValueError):
         assert channel_1 < 1
-        assert channel_1 > 1
+    with pytest.raises(ValueError):
+        assert 1 < channel_1
+
+
+def test_gt_value_error(channel_1):
+    """Ожидается исключение, если сравнивать не с Channel"""
+    with pytest.raises(ValueError):
+        assert channel_1 < 1
+    with pytest.raises(ValueError):
+        assert 1 < channel_1
 
 
 def test_get_attributes_channel(channel_1):

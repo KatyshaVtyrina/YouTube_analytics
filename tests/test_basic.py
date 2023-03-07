a8c3c516e7e7a1f7319c54352cb3e8df8e4e32b0
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-03-07 14:46:40 +0300

    change type to isinstance

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,37 +11,37 @@ def test_get_value_error():
 
 def test_get_service():
     """Ожидается получение объекта для работы с API ютуба"""
-    assert type(Basic.get_service()) is googleapiclient.discovery.Resource
+    assert isinstance(Basic.get_service(), googleapiclient.discovery.Resource)
 
 
 def test_get_channel(channel_1):
     """Ожидается dict - информация о канале """
-    assert type(channel_1.channel) is dict
+    assert isinstance(channel_1.channel, dict)
 
 
 def test_get_video(video_1):
     """Ожидается dict - информация о видео"""
-    assert type(video_1.info_video) is dict
+    assert isinstance(video_1.info_video, dict)
 
 
 def test_get_playlist(video_2):
     """Ожидается dict - информация плейлисте"""
-    assert type(video_2.playlist) is dict
+    assert isinstance(video_2.playlist, dict)
 
 
 def test_get_playlist_channel(video_2):
     """Ожидается dict - информация о плейлисте канала"""
-    assert type(video_2.playlist_channel) is dict
+    assert isinstance(video_2.playlist_channel, dict)
 
 
 def test_get_ids_videos_in_playlist(playlist_1):
     """Ожидается dict - информация о плейлисте канала"""
-    assert type(Basic._get_ids_videos_in_playlist(playlist_id=playlist_1.playlist_id)) is list
+    assert isinstance(Basic._get_ids_videos_in_playlist(playlist_id=playlist_1.playlist_id), list)
 
 
 def test_get_info_video_in_playlist(playlist_1):
     """Ожидается dict - информация о плейлисте канала"""
-    assert type(playlist_1._get_info_video_in_playlist(ids_videos=playlist_1.ids_videos)) is dict
+    assert isinstance(playlist_1._get_info_video_in_playlist(ids_videos=playlist_1.ids_videos), dict)
 
 
 def test_dict_to_json():

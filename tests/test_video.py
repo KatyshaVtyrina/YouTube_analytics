e1d65cc9c26ff61f92ff08ce4039fed7d1f40278
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-03-08 20:12:05 +0300

    add test_print_info()

--- tests/test_video.py
+++ tests/test_video.py
@@ -1,3 +1,5 @@
+import json
+
 
 def test_str(video_1, video_2):
     assert str(video_1) == f"{video_1.title}"
@@ -33,3 +35,8 @@ def test_playlist_of_channel(video_2, video_3):
     """Ожидается информация о нахождении видео в плейлисте"""
     assert (video_2.check_video_in_playlist()) == f"Видео '{video_2.title}' есть в плейлисте '{video_2.playlist_name}'"
     assert (video_3.check_video_in_playlist()) == f"Видео '{video_3.title}' нет в плейлисте '{video_3.playlist_name}'"
+
+
+def test_print_info(video_1):
+    json_data = video_1.print_info()
+    data = json.loads(json_data)
